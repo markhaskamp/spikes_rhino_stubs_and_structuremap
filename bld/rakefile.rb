bdbff4f543d2bb8da9853f34a5f234e903ed2849
Mark Haskamp <mrhask@gmail.com>
2010-08-07 15:00:16 -0400

    refactoring rakefile

--- bld/rakefile.rb
+++ bld/rakefile.rb
@@ -2,35 +2,46 @@ require 'rubygems'
 require 'albacore'
 
 rakefile = __FILE__
-home_dir = "#{rakefile}/../.."
+@home_dir = "#{rakefile}/../.."
 
-task :default => :build
+task :default => :test
 
 desc "show local variables"
 task :show do
-  puts "home_dir: \t#{home_dir}"
+  puts "@home_dir: \t#{@home_dir}"
 end
 
 desc "clean and build"
 msbuild :build => [:clean] do |msb|
   msb.properties :configuration => :Debug
   msb.targets :Build
-  msb.solution = "#{home_dir}/app/RhinoSpike/RhinoSpike.sln"
+  msb.solution = "#{@home_dir}/app/RhinoSpike/RhinoSpike.sln"
 end
 
 msbuild :clean do |msb|
   msb.properties :configuration => :Debug
   msb.targets :Clean
-  msb.solution = "#{home_dir}/app/RhinoSpike/RhinoSpike.sln"
+  msb.solution = "#{@home_dir}/app/RhinoSpike/RhinoSpike.sln"
 end
 
-desc "run mspec tests"
-task :test => [:build] do |t|
-  sh "#{home_dir}/tools_bundle/mspec/mspec --html #{home_dir}/rpt/specs.html #{home_dir}/test/RhinoSpike.Test/bin/Debug/RhinoSpike.Test.dll"
+desc "run tests"
+task :test, :output_location, :needs => :build do |t, args|
+  args.with_defaults(:output_location => :html)
+
+  if args[:output_location] == 'html' then
+    @output_target = "--html #{@home_dir}/rpt/specs.html"
+  end
+
+  run_tests_and_direct_output
 end
 
-desc "run mspec tests, send output to console"
-task :test_console => [:build] do |t|
-  sh "#{home_dir}/tools_bundle/mspec/mspec #{home_dir}/test/RhinoSpike.Test/bin/Debug/RhinoSpike.Test.dll"
+desc "show args"
+task :show_args, :arg1 do |t, args|
+  args.with_defaults(:arg1 => :html)
+  puts "args[:arg1]: [#{args[:arg1]}]"
+  puts "Args were #{args}"
 end
 
+def run_tests_and_direct_output
+  sh "#{@home_dir}/tools_bundle/mspec/mspec #{@output_target} #{@home_dir}/test/RhinoSpike.Test/bin/Debug/RhinoSpike.Test.dll"
+end
